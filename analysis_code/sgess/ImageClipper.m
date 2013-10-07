a1368d486a2872c8927104909187946f562a0a61
sgess <spencergessner@gmail.com>
2013-10-06 18:20:03 -0700

    Updated ImageClipper function to accept threshold

--- analysis_code/sgess/ImageClipper.m
+++ analysis_code/sgess/ImageClipper.m
@@ -1,4 +1,4 @@
-function [clippedImg, xx, yy, shiftVal] = ImageClipper(useImg,bufferzone)
+function [clippedImg, xx, yy, shiftVal,ty] = ImageClipper(useImg,useEnd,bufferzone,threshold)
 % Adaptive ROI function
 % Chris Eckman, SLAC, 2013
 %
@@ -10,16 +10,26 @@ function [clippedImg, xx, yy, shiftVal] = ImageClipper(useImg,bufferzone)
 %          yy         = clipped image Y axis [pixels]   
 %          shiftVal   = shifted center       [# of pixels]
 
-[m n] = size(useImg);
+[m,n] = size(useImg);
  
 mx = max(useImg);
 my = max(useImg,[],2);
 
-sorted=sort(my);
-idx=find(diff([sorted; sorted(end)-1]));
-num=[idx(1); diff(idx)];
-ty=sorted(idx(num==max(num)));
-ty = ty(end);
+if useEnd
+    my = mean(useImg(:,(end-20):end),2);
+    [~,b] = max(my);
+    mx = useImg(b,:);
+end
+
+if nargin == 4
+    ty = threshold;
+else
+    sorted=sort(my);
+    idx=find(diff([sorted; sorted(end)-1]));
+    num=[idx(1); diff(idx)];
+    ty=sorted(idx(num==max(num)));
+    ty = ty(end);
+end
 
 [k,zf] = filter([1 -1], 1, double(mx>ty));
 k(n+1:n+2-1) = zf;
@@ -27,7 +37,7 @@ P = find(k==1);
 [L, posP]= max(find(k==-1)-P);
 P = P(posP);
 tfc = [false(1,P-bufferzone) true(1,L+2*bufferzone) false(1,n-(L+P)-bufferzone)];
-yy = find(tfc);
+
 
 [k,zf] = filter([1 -1], 1, double(my>ty));
 k(m+1:m+2-1) = zf;
@@ -36,8 +46,10 @@ P = find(k==1);
 P = P(posP);
 tfr = [false(1,P-bufferzone) true(1,L+2*bufferzone) false(1,m-(L+P)-bufferzone)];
 shiftVal = P-bufferzone;
+
+tfr = tfr(1:(end-1));
+tfc = tfc(1:(end-1));
 xx = find(tfr);
+yy = find(tfc);
 
 clippedImg = useImg(tfr,tfc);
-
-
