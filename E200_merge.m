1c727cca0eea3b111c878464f548d3fa5a9d33d5
Mike Litos <mikelitos@gmail.com>
2013-11-05 20:11:10 -0800

    Added override flag to allow writing over any value on remote disk (even raw). Use sparingly, of course.

--- E200_merge.m
+++ E200_merge.m
@@ -1,25 +1,46 @@
-function data_new = E200_merge(data_src,data_dest)
+function data_new = E200_merge(data_src,data_dest,override)
+
+    % override flag -- allows writing over any field
+    if nargin<3
+        override = false;
+    elseif (override)
+        disp('WARNING: override flag has been set to TRUE.');
+        disp('You have 10 seconds to cancel (use cntl c).');
+        pause(10);
+    end
+
 	% Validate compatibility
 	merge_compatible(data_src,data_dest);
 
 	% Recursively add on fields in everything but raw
-	data_new = add_fields_main(data_src,data_dest);
+	data_new = add_fields_main(data_src,data_dest,override);
 
 end
 
 function merge_compatible(data_src,data_dest)
 	% Test version
-	if data_src.VersionInfo.Version ~= data_dest.VersionInfo.Version
-		error('Data structures are not the same version.');
-	end
+    if isfield(data_src.VersionInfo,'Version') && ...
+            isfield(data_dest.VersionInfo,'Version')
+        if data_src.VersionInfo.Version ~= data_dest.VersionInfo.Version
+            error('Data structures are not the same version.');
+        end
+    else
+        disp('WARNING: Unable to compare version number.');
+    end
 end
 
-function data_dest=add_fields_main(data_src,data_dest)
+function data_dest=add_fields_main(data_src,data_dest,override)
 	% Fields to ignore
 	% 	Note: we do want to copy raw, we just want to be strict:
 	% 	No overwriting allowed.
 	ignorefields={'VersionInfo'};
 	no_overwrite_fields={'raw'};
+    
+    % if override flag is set, over write everything
+    if (override)
+        clear ignorefields;
+        clear no_overwrite
+    end
 
 	% Get all fields
 	copy_fields=fieldnames(data_src);
@@ -28,9 +49,9 @@ function data_dest=add_fields_main(data_src,data_dest)
 	for i=1:size(copy_fields,1)
 		copystr=copy_fields{i};
 		% Ignore certain fields
-		if sum(strcmp(copystr,ignorefields))==0
+		if sum(strcmpi(copystr,ignorefields))==0
 			% Overwrite these
-			if sum(strcmp(copystr,no_overwrite_fields))==0
+			if sum(strcmpi(copystr,no_overwrite_fields))==0
 				% overwrite_bool=true;
 				overwrite_bool=true;
 			% Don't overwrite these
@@ -62,7 +83,7 @@ function [dest,diff]=copy_recurse(src,dest,overwrite_bool)
 				namestr=fieldstr_src{i};
 				sub_src=src.(namestr);
 				% Look for match
-				bool=strcmp(namestr,fieldstr_dest);
+				bool=strcmpi(namestr,fieldstr_dest);
 				% If name match is found, recurse.
 				if sum(bool)==1
 					dest.(namestr)=copy_recurse(sub_src,dest.(namestr),overwrite_bool);
