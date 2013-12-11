6df5cdded218f871d4eac2ae6df1bdb930f4925e
Joel Frederico <joelfred@slac.stanford.edu>
2013-12-11 15:17:04 -0800

    Added error on files not found.

--- add_CMOS.m
+++ add_CMOS.m
@@ -8,14 +8,25 @@ function data = add_CMOS(data,param,e_PID,e_scan_step,e_dataset)
 		% Only add if cmos cams exist.
 		% ================================================
 		if size(param.cam_CMOS,1)>0
-			display(param.cam_CMOS);
 			% ================================================
 			% Iterate for each camera
 			% ================================================
 			for i=1:size(param.cmos_path,2)
-				path = param.cmos_path{i}
+				% ================================================
+				% Gather info
+				% ================================================
+				% Get camera name
+				str=param.cam_CMOS{i,1};
+
+				% Path to dir containing files
+				path = param.cmos_path{i};
 				filelist  = dir(fullfile(path,'*.tif'));
 				n_i_shots = size(filelist,1);
+				if n_i_shots == 0
+					rgbvec = [1,0,0];
+					printstr = ['No CMOS data found for ' str '!\n'];
+					cprintf(rgbvec,printstr);
+				end
 				step      = zeros(1,n_i_shots);
 				pID       = zeros(1,n_i_shots);
 				files     = cell(1,n_i_shots);
@@ -29,15 +40,13 @@ function data = add_CMOS(data,param,e_PID,e_scan_step,e_dataset)
 					step(j)  = str2num(file(separ(2)+1:separ(3)-1));
 					pID(j)   = str2num(file(separ(4)+1:end-4));
 				end
+
+				% Camera type is CMOS.
+				format=cell_construct('CMOS',1,n_i_shots);
 	
 				% ================================================
 				% Write information gathered into the struct
 				% ================================================
-				% Camera type is CMOS.
-				format=cell_construct('CMOS',1,n_i_shots);
-	
-				% Get camera name
-				str=param.cam_CMOS{i,1}
 	
 				% Pass through cam name converter (should do nothing?)
 				[filestr,structstr]=cams2filenames(str,param.timestamp);
