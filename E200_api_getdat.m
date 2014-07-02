7451bd8cbeaebe80c3d356769cc744307b0f15db
Joel Frederico <joelfred@slac.stanford.edu>
2014-07-01 21:36:05 -0700

    Updated to handle cell arrays vs num arrays

--- E200_api_getdat.m
+++ E200_api_getdat.m
@@ -1,7 +1,11 @@
 function out = E200_api_getdat(struct,UID,varargin)
-	bool = ( struct.UID == UID );
-	if nargin == 0
-		out  = struct.dat{bool};
+	bool = ismember(struct.UID,UID);
+	if nargin == 2
+		if strcmp(class(struct.dat),'cell')
+			out  = struct.dat{bool};
+		else
+			out  = struct.dat(bool);
+		end
 	else
 		varargin{1}
 		out  = struct.(varargin{1})(bool);
