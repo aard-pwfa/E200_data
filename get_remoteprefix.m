8b3195cd3a4047abaf303b799cffff13644720f3
Joel Frederico <joelfred@slac.stanford.edu>
2015-07-11 16:50:46 -0700

    Changed default to PWFA_5big, formatting, documentation

--- get_remoteprefix.m
+++ get_remoteprefix.m
@@ -1,21 +1,36 @@
 function prefix=get_remoteprefix()
-	def_prefix='/Volumes/PWFA_4big';
+	def_prefix = '/Volumes/PWFA_5big';
 
-	% Get existing prefix, or default
+    % ===============================
+	% Get existing prefix, or set
+    % the default prefix
+    % ===============================
 	if ispref('FACET_data','prefix')
-		prefix=getpref('FACET_data','prefix');
+		prefix = getpref('FACET_data','prefix');
 	else
 		setpref('FACET_data','prefix',def_prefix);
-		prefix=def_prefix;       
+		prefix = def_prefix;
 	end
 
-	datapath=fullfile(prefix,'nas','nas-li20-pm00');
+    % ===============================
+	% This part always exists
+    % ===============================
+	datapath = fullfile(prefix,'nas','nas-li20-pm00');
+
+    % ===============================
+	% Check if the path works
+    % ===============================
 	if exist(datapath)~=7
-		% Drive may not be mounted: warn user, allow to mount drive or change prefix.
+        % ===============================
+		% Drive may not be mounted
+        % ===============================
 		options={'Try again.','Locate folder containing /nas.'};
 		button=options{1};
 
-		% While the user tries again, and the datapath isn't a folder
+        % ===============================
+		% While the user tries again,
+        % and the datapath isn't a folder
+        % ===============================
 		while strcmp(button,options{1}) && exist(datapath)~=7
 			button=questdlg(sprintf(['WARNING: Path to data doesn''t exist:\n\n' datapath '\n\nDrive may not be mounted.']),'Data Not Found',options{1},options{2},options{1});
 			if strcmp(button,options{2})
